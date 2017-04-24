ad696f399a2bade09cf8228bd54c9001d5b358eb
HenrySu <henry903007@gmail.com>
2017-04-24 23:39:03 +0800

    implement layout switching between two cells in favorite page

--- FavoriteVC.swift
+++ FavoriteVC.swift
@@ -19,10 +19,27 @@ class FavoriteVC: UIViewController {
     var favoriteEvents = [String: [ClubEvent]]() // "2017, 4" : []
     var eventDateSections = [String]()
     
+    var isBigCellLayout = true
     
     let loadingView = LoadingIndicator()
 
 
+    @IBAction func layoutSwitchDidClick(_ sender: UIBarButtonItem) {
+        
+        if isBigCellLayout {
+            isBigCellLayout = false
+            sender.image = UIImage(named: "layout-big-cell")
+
+
+        }
+        else {
+            isBigCellLayout = true
+            sender.image = UIImage(named: "layout-small-cell")
+
+        }
+        loadUserFavoriteEvents()
+//        tbvFavoriteEvents.reloadData()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,8 +51,10 @@ class FavoriteVC: UIViewController {
         
         searchBar.backgroundImage = UIImage()
 
-        
         loadUserFavoriteEvents()
+        
+        self.hideKeyboardWhenTappedAround()
+
 
     }
 
@@ -44,7 +63,7 @@ class FavoriteVC: UIViewController {
     func loadUserFavoriteEvents() {
         
         loadingView.showLoading(in: self.tbvFavoriteEvents)
-
+        
         APIManager.shared.getUserData { (json) in
             if json != nil {
                 self.favoriteEvents = [:]
@@ -56,30 +75,25 @@ class FavoriteVC: UIViewController {
                             if let monthSection = event["time"].string {
                                 
                                 let clubEvent = ClubEvent(json: event)
-                
+                                
                                 if self.favoriteEvents[monthSection] == nil {
                                     self.eventDateSections.append(monthSection)
                                     self.favoriteEvents[monthSection] = []
                                 }
                                 self.favoriteEvents[monthSection]?.append(clubEvent)
                             }
-
-
                             
                         }
-                        print(self.eventDateSections)
-                        print(self.favoriteEvents)
-
                         self.tbvFavoriteEvents.reloadData()
                     }
                     
-                        self.loadingView.hideLoading()
+                    self.loadingView.hideLoading()
                     
                 }
             }
         }
-
-    
+        
+        
     }
     
 }
@@ -99,19 +113,41 @@ extension FavoriteVC: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifierBig, for: indexPath) as! ClubEventBigCell
         
+        var cell: UITableViewCell
         let clubEvent: ClubEvent
         
         // get section then get event in that section
         clubEvent = (favoriteEvents[eventDateSections[indexPath.section]]?[indexPath.row])!
         
-        cell.lbSchool.text = clubEvent.schoolName ?? "神秘學校"
-        cell.lbClub.text = clubEvent.clubName ?? "神秘社團"
-        cell.lbEvent.text = clubEvent.name!
-        cell.lbLocation.text = clubEvent.location
-        cell.lbTime.text = "\(clubEvent.startDate!) - \(clubEvent.endDate!) / \(clubEvent.startTime!) - \(clubEvent.endTime!)"
-        Utils.loadImageFromURL(imageView: cell.imgThumbnail, urlString: clubEvent.imageURL!)
+
+        if isBigCellLayout {
+            let bigCell: ClubEventBigCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifierBig, for: indexPath) as! ClubEventBigCell
+            
+            bigCell.lbSchool.text = clubEvent.schoolName ?? "神秘學校"
+            bigCell.lbClub.text = clubEvent.clubName ?? "神秘社團"
+            bigCell.lbEvent.text = clubEvent.name!
+            bigCell.lbLocation.text = clubEvent.location
+            bigCell.lbTime.text = "\(clubEvent.startDate!) - \(clubEvent.endDate!) / \(clubEvent.startTime!) - \(clubEvent.endTime!)"
+            Utils.loadImageFromURL(imageView: bigCell.imgThumbnail, urlString: clubEvent.imageURL!)
+            
+            cell = bigCell
+        }
+        else {
+            let smallCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifierSmall, for: indexPath) as! ClubEventSmallCell
+        
+            smallCell.lbSchool.text = clubEvent.schoolName ?? "神秘學校"
+            smallCell.lbClub.text = clubEvent.clubName ?? "神秘社團"
+            smallCell.lbEvent.text = clubEvent.name!
+            smallCell.lbTime.text = clubEvent.startTime!
+            if let startDate = clubEvent.startDate {
+                let day = String(startDate.characters.suffix(2))
+                smallCell.imgDate.image = UIImage(named: day)
+            }
+            
+            cell = smallCell
+        }
+
         
         // Setup cell style
         cell.layer.cornerRadius = 3
@@ -119,9 +155,17 @@ extension FavoriteVC: UITableViewDataSource, UITableViewDelegate {
         return cell
     }
     
-
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if isBigCellLayout {
+            return 265
+        }
+        else {
+            return 74
+        }
+    }
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 40
+        return 32
     }
     
     
@@ -138,3 +182,4 @@ extension FavoriteVC: UITableViewDataSource, UITableViewDelegate {
     }
     
 }
+
